826590adc22655898203cd46781bdf082bec8410
Student <student@Makerss-MacBook-Pro-8.local>
2019-09-30 14:52:43 +0100

    write method to return text stored in a note

--- note-model-tests.js
+++ note-model-tests.js
@@ -3,11 +3,37 @@
     var note = new Note("JavaScript is GREAT!");
 
     if (note.text !== "JavaScript is GREAT!") {
-    throw new Error("Text doesn't match look at your code.");
-  } else {
-    console.log("You have a gold star")
-  }
-};
+      throw new Error("Text doesn't match look at your code.");
+    } else {
+      console.log("You have a gold star")
+    }
+  };
 
-testNotesTextStoresString();
+  testNotesTextStoresString();
+
+  function testNotesTextReturnsText() {
+    var note = new Note("JavaScript is GREAT!");
+
+    if (note.showNoteText() !== "JavaScript is GREAT!") {
+      throw new Error("Text doesn't match look at your code.");
+    } else {
+      console.log("You have a gold star")
+    }
+  };
+
+  testNotesTextReturnsText();
 })(this);
+
+// (function(exports) {
+//   function testNotesTextReturnsText() {
+//     var note = new Note("JavaScript is GREAT!");
+//
+//     if (note.showNoteText() !== "JavaScript is GREAT!") {
+//       throw new Error("Text doesn't match look at your code.");
+//     } else {
+//       console.log("You have a gold star")
+//     }
+//   };
+//
+//   testNotesTextReturnsText();
+// })(this);
--- note-model.js
+++ note-model.js
@@ -1,6 +1,10 @@
 (function(exports) {
-function Note(string) {
-  this.text = string;
-};
+  function Note(string) {
+    this.text = string;
+  };
+
+  Note.prototype.showNoteText = function() {
+    return this.text;
+  };
   exports.Note = Note;
 })(this);
